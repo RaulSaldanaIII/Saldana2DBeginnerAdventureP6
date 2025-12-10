fe85905e65ed7e8727035cc6b7a6f04bb7a6bcac
RaulSaldanaIII <756853@student.dusd.net>
2025-12-10 13:47:08 -0800

    Added UI
    Finish "The Game Controller" script

--- SaldanaFinalFlappyP6/Assets/Scripts/Bird.cs
+++ SaldanaFinalFlappyP6/Assets/Scripts/Bird.cs
@@ -7,10 +7,12 @@ public class Bird : MonoBehaviour
     private bool IsDead = false;
     private Rigidbody2D rb2d;
     public float upforce = 200f;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim= GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,11 +24,14 @@ public class Bird : MonoBehaviour
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upforce));
+                anim.SetTrigger("Flap");
             }
         }
     }
     void OnCollisionEnter2D ()
     {
         IsDead = true;
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
     }
 }
