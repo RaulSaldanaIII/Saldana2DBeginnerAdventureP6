c63dae89180ad6fb5a7f441d8b8f7cbc6f653671
RaulSaldanaIII <756853@student.dusd.net>
2025-12-18 10:27:42 -0800

    almost
    .....

--- SaldanaFinalFlappyP6/Assets/Scripts/GameControl.cs
+++ SaldanaFinalFlappyP6/Assets/Scripts/GameControl.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour
 {
     public static GameControl instance;
     public GameObject gameOverText;
+    public Text scoreText;
     public bool gameOver = false;
     public float scrollSpeed = -1.5f;
+    private int score = 0;
     // Start is called before the first frame update
     void Awake ()
     {
@@ -29,6 +32,15 @@ public class GameControl : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+    }
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score:" + score.ToString();
     }
 
     public void BirdDied()
